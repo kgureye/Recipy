bc2bd74fb35cfc43c62489ce620adeb7d1c02af6
Kani <kanigureye@outlook.com>
2022-07-31 18:49:42 +0200

    "Added Cuisine component and routing"

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Home from "./Home";
+import {Route, Routes } from 'react-router-dom';
+import Cuisine from './Cuisine';
 
 function Pages() {
   return (
-    <div>
-        <Home />
-    </div>
+      <Routes>
+        {/* If the path matches / which is the homepage, then render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="/cuisine/:type" element={<Cuisine />} /> {/* adding ":/:type" allows us to go to cuisine/italian and so on. */}
+      </Routes>
   )
 }
 
