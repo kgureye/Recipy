957cf42200b6ec6d5be4ec60fdae399722e4976c
Kani <kanigureye@outlook.com>
2022-08-01 20:23:31 +0200

    "added search functionality"

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Home from "./Home";
 import {Route, Routes } from 'react-router-dom';
+import Home from "./Home";
 import Cuisine from './Cuisine';
+import Searched from './Searched';
 
 function Pages() {
   return (
@@ -9,6 +10,7 @@ function Pages() {
         {/* If the path matches / which is the homepage, then render the Home component */}
           <Route path="/" element={<Home />} />
           <Route path="/cuisine/:type" element={<Cuisine />} /> {/* adding ":/:type" allows us to go to cuisine/italian and so on. */}
+          <Route path="/searched/:search" element={<Searched />} />
       </Routes>
   )
 }
